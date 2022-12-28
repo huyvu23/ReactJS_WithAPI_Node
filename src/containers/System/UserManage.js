d0dd59072348cf64c5f766a394230918b343bbc8
Huy Vũ <93518371+huyvu23@users.noreply.github.com>
2022-12-28 10:45:43 +0700

    [ADD] - video 38 hien thi du lieu ra view

--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -1,33 +1,70 @@
-import React, { Component } from 'react';
-import { FormattedMessage } from 'react-intl';
-import { connect } from 'react-redux';
-class UserManage extends Component {
+import React, { useState, useEffect } from "react";
+import { FormattedMessage } from "react-intl";
+import { connect } from "react-redux";
+import "./UserManager.scss";
+import { getAllUsers } from "../../services/userService";
 
-    state = {
+function UserManage(props) {
+  const [arrUsers, setArrUsers] = useState([]);
+  useEffect(() => {
+    getListUsers();
+  }, []);
 
+  const getListUsers = async () => {
+    let response = await getAllUsers("ALL");
+    if (response && response.errCode === 0) {
+      setArrUsers(response.user);
     }
+  };
 
-    componentDidMount() {
-
-    }
-
-
-    render() {
-        return (
-            <div className="text-center">Manage users</div>
-        );
-    }
-
+  return (
+    <>
+      <div className="users-container">
+        <div className="title text-center">Manage users with Huy</div>
+        {/* Table */}
+        <div className="users-table mt-3 mx-1">
+          <table id="customers">
+            <tr>
+              <th>Email</th>
+              <th>LastName</th>
+              <th>FirstName</th>
+              <th>Address</th>
+              <th>Actions</th>
+            </tr>
+            {arrUsers &&
+              arrUsers.map((user) => {
+                return (
+                  <>
+                    <tr key={user.id}>
+                      <td>{user.email}</td>
+                      <td>{user.lastName}</td>
+                      <td>{user.firstName}</td>
+                      <td>{user.address}</td>
+                      <td>
+                        <button className="btn-edit">
+                          <i className="fas fa-pencil-alt" />
+                        </button>
+                        <button className="btn-delete">
+                          <i className="fas - fa-trash" />
+                        </button>
+                      </td>
+                    </tr>
+                  </>
+                );
+              })}
+          </table>
+        </div>
+      </div>
+    </>
+  );
 }
 
-const mapStateToProps = state => {
-    return {
-    };
+const mapStateToProps = (state) => {
+  return {};
 };
 
-const mapDispatchToProps = dispatch => {
-    return {
-    };
+const mapDispatchToProps = (dispatch) => {
+  return {};
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(UserManage);
